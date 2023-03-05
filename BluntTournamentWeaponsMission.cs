c421501c3b9b0508ee6b0576d76833f36e44dd38
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2023-03-05 22:21:11 +0800

    Fixed a bug where the player's voice could not be heard when giving commands in battle.

--- BluntTournamentWeaponsMission.cs
+++ BluntTournamentWeaponsMission.cs
@@ -8,28 +8,21 @@ namespace BluntTournamentWeapons
     [HarmonyPatch(typeof(Mission))]
     public class BluntTournamentWeaponsMission
     {
+        private static bool Prepare(Mission __instance) => __instance != null && __instance.HasMissionBehavior<ArenaAgentStateDeciderLogic>();
+
         // Disable melee blood splatters in tournaments and practice fights.
         [HarmonyPostfix]
         [HarmonyPatch("MeleeHitCallback")]
-        internal static void Postfix1(Mission __instance, ref HitParticleResultData hitParticleResultData)
+        internal static void Postfix1(ref HitParticleResultData hitParticleResultData)
         {
-            if (__instance.HasMissionBehavior<ArenaAgentStateDeciderLogic>())
-            {
-                hitParticleResultData.StartHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
-                hitParticleResultData.ContinueHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
-                hitParticleResultData.EndHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
-            }
+            hitParticleResultData.StartHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
+            hitParticleResultData.ContinueHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
+            hitParticleResultData.EndHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
         }
 
         // Disable missile blood splatters in tournaments and practice fights.
         [HarmonyPostfix]
         [HarmonyPatch("MissileHitCallback")]
-        internal static void Postfix2(Mission __instance, ref int extraHitParticleIndex)
-        {
-            if (__instance.HasMissionBehavior<ArenaAgentStateDeciderLogic>())
-            {
-                extraHitParticleIndex = -1;
-            }
-        }
+        internal static void Postfix2(ref int extraHitParticleIndex) => extraHitParticleIndex = -1;
     }
 }
